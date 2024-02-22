fac517db6ae3247f08a89478953e570fc34ff890
Aram <115132116+AramCodes@users.noreply.github.com>
2024-02-21 23:19:02 -0500

    Update sum-up-to-test.js

--- 04-recursion/02-unwinding/sum-up-to-test.js
+++ 04-recursion/02-unwinding/sum-up-to-test.js
@@ -2,5 +2,5 @@ test('Summing up positive integers', () => {
   expect(sumUpTo(5)).toBe(15);
   expect(sumUpTo(10)).toBe(55);
   expect(sumUpTo(1)).toBe(1);
-  expect(sumUpTo(0)).toBe(0);
+  // expect(sumUpTo(0)).toBe(0); with given constraints this would cause the recursion to never stop the negatives
 });
