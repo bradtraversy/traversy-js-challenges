835f37be63ee5bb106b0c9ef3231caeb0818c883
Vighnesh Manjrekar <vighneshmanjrekar4193@gmail.com>
2024-05-26 15:16:37 +0000

    Fix: Unwind test file missing import and solution file test case for 0

--- 04-recursion/02-unwinding/sum-up-to-solution.js
+++ 04-recursion/02-unwinding/sum-up-to-solution.js
@@ -1,9 +1,10 @@
 function sumUpTo(n) {
   // Base case - when n is 1, we return 1
-  if (n === 1) {
-    return 1;
+  // Test case - when n is 0, we return 0
+  if (n === 1 || n === 0) {
+    return n;
   }
-
+  
   // Recursive case - when n is greater than 1, we return the sum of n and sumUpTo(n - 1)
   return n + sumUpTo(n - 1);
 }
